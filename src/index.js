03ce769f6f9be93f7e3cdf2bae0666338953f179
Shir Magen <shirmagen095@gmail.com>
2019-03-30 02:49:30 +0300

    wip

--- src/index.js
+++ src/index.js
@@ -1,38 +1,11 @@
-var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-   
-    var sql = require("mssql");
-
-    // config for your database
-    var config = {
-        // user: 'user',
-        // password: 'mypassword',
-        server: 'user/sqlexpress', 
-        database: 'migration-tool' 
-    };
-
-    // connect to your database
-    sql.connect(config, function (err) {
-    
-        if (err) console.log(err);
-
-        // create Request object
-        var request = new sql.Request();
-           
-        // query to the database and get the records
-        request.query('select * from Student', function (err, recordset) {
-            
-            if (err) console.log(err)
-
-            // send records as a response
-            res.send(recordset);
-            
-        });
-    });
-});
-
-var server = app.listen(5000, function () {
-    console.log('Server is running..');
-});
+const sql = require('mssql')
+ 
+async () => {
+    try {
+        await sql.connect('mssql://username:password@localhost/database')
+        const result = await sql.query`select * from mytable where id = ${value}`
+        console.dir(result)
+    } catch (err) {
+        // ... error checks
+    }
+}
