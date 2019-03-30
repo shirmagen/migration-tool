0470eab1353b1272398aeaf04df670e529dd5058
Shir Magen <shirmagen095@gmail.com>
2019-03-30 14:05:49 +0300

    db connected succesfully

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-const sql = require('mssql')
- 
-async () => {
-    try {
-        await sql.connect('mssql://username:password@localhost/database')
-        const result = await sql.query`select * from mytable where id = ${value}`
-        console.dir(result)
-    } catch (err) {
-        // ... error checks
-    }
-}
+const sql = require("mssql/msnodesqlv8");
+const conn = new sql.ConnectionPool({
+  database: "migration-tool",
+  server: "localhost\\SQLEXPRESS",
+  driver: "msnodesqlv8",
+  options: {
+    trustedConnection: true
+  }
+});
+conn.connect().then(() => {
+    console.log('connected')
+  // ... sproc call, error catching, etc
+  // example: https://github.com/patriksimek/node-mssql#request
+});
