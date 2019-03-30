5f72aaf0d05694841ac512a5e730b5e89bad0b49
Shir Magen <shirmagen095@gmail.com>
2019-03-30 14:38:26 +0300

    query succeeded

--- src/index.js
+++ src/index.js
@@ -9,6 +9,16 @@ const conn = new sql.ConnectionPool({
 });
 conn.connect().then(() => {
     console.log('connected')
+    const request = new sql.Request(conn);
+
+    request.query('select * from dbo.Basket', function (err, recordset) {
+        if (err)
+            console.log(err);
+        else
+        console.log(recordset.recordsets[0]);
+        conn.close();
+    });
+    
   // ... sproc call, error catching, etc
   // example: https://github.com/patriksimek/node-mssql#request
 });
