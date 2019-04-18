5b9a3dbb9d0483345ec5fc0654077e308d51ddcd
Shir Magen <shirmagen095@gmail.com>
2019-04-19 00:09:14 +0300

    moved connections to index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import {migrateCollection} from './migration/migration.controller';
 import {ctor as BasketCtor, Basket} from './mongo/basket/basket.model';
 import {ctor as BananaCtor, Banana} from './mongo/banana/banana.model';
+import {getSqlConnectionObject, connectSqlDb, createRequest, disconnectSqlDb} from './sql/sql-connection';
+import {connectMongoDb, disconnectMongoDb} from './mongo/mongo-connection';
 
 const migrate = async () => {
     try {
-        
-        migrateCollection('Basket', BasketCtor, Basket);
-        migrateCollection('Banana', BananaCtor, Banana);
+        const sqlConnection = getSqlConnectionObject();
+        await connectSqlDb(sqlConnection);
+        const sqlRequest = createRequest(sqlConnection);
+        await connectMongoDb();
+        await migrateCollection('Basket', sqlRequest, BasketCtor, Basket);
+        await migrateCollection('Banana', sqlRequest, BananaCtor, Banana);
+        await disconnectSqlDb(sqlConnection);
+        await disconnectMongoDb();
 
     } catch (err) {
         console.log('caught an error:' + err);
--- src/migration/migration.controller.js
+++ src/migration/migration.controller.js
@@ -1,16 +1,8 @@
 import {getAll} from '../sql/base.controller';
-import {connectMongoDb, disconnectMongoDb} from '../mongo/mongo-connection';
-import {connectSqlDb, getSqlConnectionObject, createRequest, disconnectSqlDb} from '../sql/sql-connection'
 
-export const migrateCollection = async (collectionNameInSql, ctor, model) => {
-    const sqlConnection = getSqlConnectionObject();
-    await connectSqlDb(sqlConnection);
-    const sqlRequest = createRequest(sqlConnection);
+export const migrateCollection = async (collectionNameInSql, sqlRequest, ctor, model) => {
     const allObjsInTable = await getAll(sqlRequest, collectionNameInSql);
-    await disconnectSqlDb(sqlConnection);
-    await connectMongoDb();
     await saveSqlObjsInMongo(allObjsInTable, ctor, model);
-    disconnectMongoDb();
 };
 
 const saveSqlObjsInMongo = async (allObjsInTable, ctor, model) => {
@@ -19,15 +11,3 @@ const saveSqlObjsInMongo = async (allObjsInTable, ctor, model) => {
         await model.create(mongoDoc);
      }));
 };
-
-async function setConnections(sqlConnection, sqlRequest) {
-    sqlConnection = getSqlConnectionObject();
-    await connectSqlDb(sqlConnection);
-    sqlRequest = createRequest(sqlConnection);
-    connectMongoDb();
-};
-
-const closeConnections = async (sqlConnection) => {
-    disconnectSqlDb(sqlConnection);
-    disconnectMongoDb();
-;}
